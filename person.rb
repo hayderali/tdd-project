4fdccc3e7dde5fb8506eef6aaea570ef3bf21d01
Hayder Ali <hali@spartaglobal.com>
2015-11-18 14:56:58 +0000

    Test 3 Pass - View First 2 Emails

--- person.rb
+++ person.rb
@@ -2,13 +2,19 @@ require 'date'
 
 class Person
   attr_accessor :dob, :first_name, :surname, :fullname  
+  attr_reader :mobile_number, :emails
   def initialize(fname,sname,dob)
     @dob = Date.parse(dob)
     @first_name =fname.capitalize
     @surname = sname.capitalize
+    @emails = []
   end
 
   def fullname
     "#{first_name} #{surname}"
   end
+  
+  def add_email(input)
+    @emails.push(input)
+  end 
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -11,5 +11,23 @@ describe "Person Class - entries in phonebook" do
     expect(person.fullname).to eq "Joe Bloggs"
   end  
   
+  end
+  
+describe "Person Class - entries in phonebook" do
+  it "check email array is working" do
+    person = Person.new("joe","bloggs","1 Jan 1990")
+    
+    expect(person.emails).to eq []
+  
+    
+  end 
 end
 
+describe "Person Class - entries in phonebook" do
+  it "view first 2 emails" do
+    person = Person.new("joe","bloggs","1 Jan 1990")
+    person.add_email("joe@foo.com")
+    person.add_email("joe.bloggs@work.com")
+    expect(person.emails).to eq ["joe@foo.com","joe.bloggs@work.com"]   
+  end 
+end
